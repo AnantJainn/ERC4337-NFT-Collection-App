c7b2b285f62cb5d331d0b1e046d090f367b0e4fd
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-04-30 10:54:54 +0530

    token balance update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,19 +5,22 @@ import {
   useDisconnect,
   useContract,
   useClaimToken,
+  useTokenBalance,
+  // useReadContract,
 } from "@thirdweb-dev/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { TOKEN_ADDRESS } from "../constant/addresses";
+// import { resolveMethod } from "thirdweb";
 export default function Navbar() {
   const address = useAddress();
   const disconnect = useDisconnect();
   const toast = useToast();
 
-  // Fetch ERC-20 contract
   const { contract: erc20Contract } = useContract(TOKEN_ADDRESS);
-
+  const { data: tokenBalance, isLoading: isTokenBalanceLoading } =
+    useTokenBalance(erc20Contract, address);
   // Claim ERC-20 tokens
   const {
     mutate: claimTokens,
@@ -25,11 +28,15 @@ export default function Navbar() {
     error,
   } = useClaimToken(erc20Contract);
 
+  // State to track if tokens have been claimed for the current user
+  const [tokensClaimed, setTokensClaimed] = useState(false);
   useEffect(() => {
-    if (address) {
+    if (address && !tokensClaimed) {
+      // Check if address is available and tokens haven't been claimed yet
       const claim = async () => {
         try {
           await claimTokens({ to: address, amount: 20 });
+          setTokensClaimed(true); // Update state to indicate tokens have been claimed
           toast({
             title: "Tokens Claimed!",
             description: "You have successfully claimed ERC-20 tokens.",
@@ -51,13 +58,46 @@ export default function Navbar() {
 
       claim();
     }
-  }, [address, claimTokens, toast]);
+  }, [address, claimTokens, toast, tokensClaimed]);
+  // useEffect(() => {
+  //   if (address && tokenBalance?.displayValue === "0") {
+  //     console.log("tb", tokenBalance);
+  //     // Check if address is available, tokens haven't been claimed yet,
+  //     // and token balance is zero
+  //     const claim = async () => {
+  //       try {
+  //         await claimTokens({ to: address, amount: 20 });
+  //         setTokensClaimed(true); // Update state to indicate tokens have been claimed
+  //         toast({
+  //           title: "Tokens Claimed!",
+  //           description: "You have successfully claimed ERC-20 tokens.",
+  //           status: "success",
+  //           duration: 5000,
+  //           isClosable: true,
+  //         });
+  //       } catch (err) {
+  //         const errorMessage = (err as Error).message; // Type assertion
+  //         toast({
+  //           title: "Error",
+  //           description: "Failed to claim tokens: " + errorMessage,
+  //           status: "error",
+  //           duration: 5000,
+  //           isClosable: true,
+  //         });
+  //       }
+  //     };
+
+  //     claim();
+  //   }
+  // }, [address, claimTokens, toast, tokensClaimed, tokenBalance]);
+
   return (
     <Container maxW={"1200px"} py={5}>
       <Flex justifyContent={"space-between"} alignItems={"center"}>
         <Text fontWeight={"bold"} fontSize={30}>
           Raindrops X
         </Text>
+        <Text>Token Balance: {tokenBalance?.displayValue}</Text>
         {!address ? (
           <ConnectWallet
             className={styles.walletButton}
