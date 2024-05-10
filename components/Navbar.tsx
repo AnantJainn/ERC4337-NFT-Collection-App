bf6adadb9dc7ffa0c9e78d12e6596260bea9c2bf
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-05-10 17:24:32 +0530

    navbar of nft page made responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,13 +6,12 @@ import {
   useContract,
   useClaimToken,
   useTokenBalance,
-  // useReadContract,
 } from "@thirdweb-dev/react";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { TOKEN_ADDRESS } from "../constant/addresses";
-// import { resolveMethod } from "thirdweb";
+
 export default function Navbar() {
   const address = useAddress();
   const disconnect = useDisconnect();
@@ -21,86 +20,19 @@ export default function Navbar() {
   const { contract: erc20Contract } = useContract(TOKEN_ADDRESS);
   const { data: tokenBalance, isLoading: isTokenBalanceLoading } =
     useTokenBalance(erc20Contract, address);
-  // Claim ERC-20 tokens
   const {
     mutate: claimTokens,
     isLoading,
     error,
   } = useClaimToken(erc20Contract);
 
-  // State to track if tokens have been claimed for the current user
   const [tokensClaimed, setTokensClaimed] = useState(false);
-  // useEffect(() => {
-  //   if (address && !tokensClaimed) {
-  //     // Check if address is available and tokens haven't been claimed yet
-  //     const claim = async () => {
-  //       try {
-  //         await claimTokens({ to: address, amount: 20 });
-  //         setTokensClaimed(true); // Update state to indicate tokens have been claimed
-  //         toast({
-  //           title: "Tokens Claimed!",
-  //           description: "You have successfully claimed ERC-20 tokens.",
-  //           status: "success",
-  //           duration: 5000,
-  //           isClosable: true,
-  //         });
-  //       } catch (err) {
-  //         const errorMessage = (err as Error).message; // Type assertion
-  //         toast({
-  //           title: "Error",
-  //           description: "Failed to claim tokens: " + errorMessage,
-  //           status: "error",
-  //           duration: 5000,
-  //           isClosable: true,
-  //         });
-  //       }
-  //     };
-
-  //     claim();
-  //   }
-  // }, [address, claimTokens, toast, tokensClaimed]);
-  // useEffect(() => {
-  //   if (address && tokenBalance?.displayValue === "0") {
-  //     console.log("tb", tokenBalance);
-  //     // Check if address is available, tokens haven't been claimed yet,
-  //     // and token balance is zero
-  //     const claim = async () => {
-  //       try {
-  //         await claimTokens({ to: address, amount: 20 });
-  //         setTokensClaimed(true); // Update state to indicate tokens have been claimed
-  //         toast({
-  //           title: "Tokens Claimed!",
-  //           description: "You have successfully claimed ERC-20 tokens.",
-  //           status: "success",
-  //           duration: 5000,
-  //           isClosable: true,
-  //         });
-  //       } catch (err) {
-  //         const errorMessage = (err as Error).message; // Type assertion
-  //         toast({
-  //           title: "Error",
-  //           description: "Failed to claim tokens: " + errorMessage,
-  //           status: "error",
-  //           duration: 5000,
-  //           isClosable: true,
-  //         });
-  //       }
-  //     };
-
-  //     claim();
-  //   }
-  // }, [address, claimTokens, toast, tokensClaimed, tokenBalance]);
   useEffect(() => {
-    console.log("address:", address);
-    console.log("tokenBalance:", tokenBalance);
     if (address && tokenBalance?.displayValue === "0.0") {
-      console.log("Claiming tokens...");
-      // Check if address is available, tokens haven't been claimed yet,
-      // and token balance is zero
       const claim = async () => {
         try {
           await claimTokens({ to: address, amount: 10 });
-          setTokensClaimed(true); // Update state to indicate tokens have been claimed
+          setTokensClaimed(true);
           toast({
             title: "Tokens Claimed!",
             description: "You have successfully claimed ERC-20 tokens.",
@@ -109,7 +41,7 @@ export default function Navbar() {
             isClosable: true,
           });
         } catch (err) {
-          const errorMessage = (err as Error).message; // Type assertion
+          const errorMessage = (err as Error).message;
           toast({
             title: "Error",
             description: "Failed to claim tokens: " + errorMessage,
@@ -119,40 +51,64 @@ export default function Navbar() {
           });
         }
       };
-
       claim();
     }
   }, [address, claimTokens, toast, tokensClaimed, tokenBalance]);
 
   return (
     <Container maxW={"1200px"} py={5}>
-      <Flex justifyContent={"space-between"} alignItems={"center"}>
-        <Text fontWeight={"bold"} fontSize={30}>
+      <Flex
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        flexDirection={{ base: "column", md: "row" }} // Flex direction based on screen size
+      >
+        <Text fontWeight={"bold"} fontSize={{ base: 20, md: 30 }}>
+          {" "}
+          {/* Font size based on screen size */}
           Raindrops X
         </Text>
-        <Text fontSize={20}>Token Balance: {tokenBalance?.displayValue}</Text>
-        {!address ? (
-          <ConnectWallet
-            className={styles.walletButton}
-            btnTitle="Sign In"
-            theme="light"
-          />
-        ) : (
-          <Flex alignItems="center">
-            <Link href={`/profile/${address}`} passHref>
-              <Text mr={4} cursor="pointer">
-                Profile
-              </Text>
-            </Link>
-            <Text mr={4} cursor="pointer" onClick={disconnect}>
-              Sign Out
-            </Text>
-            <Avatar
-              size={"sm"}
-              src={`https://avatars.dicebear.com/api/avataaars/${address}.svg`}
+        <Flex alignItems="center">
+          {!address ? (
+            <ConnectWallet
+              className={styles.walletButton}
+              btnTitle="Sign In"
+              theme="light"
             />
-          </Flex>
-        )}
+          ) : (
+            <>
+              <Link href={`/profile/${address}`} passHref>
+                <Text
+                  mr={{ base: 0, md: 4 }}
+                  mb={{ base: 2, md: 0 }}
+                  cursor="pointer"
+                >
+                  {" "}
+                  {/* Margin based on screen size */}
+                  Profile
+                </Text>
+              </Link>
+              <Text
+                mr={{ base: 0, md: 4 }}
+                mb={{ base: 2, md: 0 }}
+                cursor="pointer"
+                onClick={disconnect}
+              >
+                {" "}
+                {/* Margin based on screen size */}
+                Sign Out
+              </Text>
+              <Avatar
+                size={"sm"}
+                src={`https://avatars.dicebear.com/api/avataaars/${address}.svg`}
+              />
+            </>
+          )}
+          <Text fontSize={{ base: 12, md: 20 }} ml={{ base: 2, md: 4 }}>
+            {" "}
+            {/* Font size and margin based on screen size */}
+            Token Balance: {tokenBalance?.displayValue}
+          </Text>
+        </Flex>
       </Flex>
     </Container>
   );
