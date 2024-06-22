ab5d5aabea69eba85f847f6e8b85e3eff1f837f1
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-06-22 16:28:33 +0530

    cards UI improved

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import {
   Web3Button,
   useAddress,
   useContract,
   useNFT,
-  useClaimToken,
 } from "@thirdweb-dev/react";
-import { NFT_ADDRESS, TOKEN_ADDRESS } from "../constant/addresses";
+import { NFT_ADDRESS } from "../constant/addresses";
 
 type Props = {
   tokenId: string;
@@ -16,46 +15,40 @@ type Props = {
 const NFTCard: React.FC<Props> = ({ tokenId }) => {
   const address = useAddress();
   const { contract: nftContract } = useContract(NFT_ADDRESS);
-  // const { contract: erc20Contract } = useContract(TOKEN_ADDRESS);
-
-  // console.log("ERC20", erc20Contract);
-  console.log("Address", address);
-
-  // const {
-  //   mutate: claimTokens,
-  //   isLoading,
-  //   error,
-  // } = useClaimToken(erc20Contract);
-
-  const { contract } = useContract(NFT_ADDRESS);
   const { data } = useNFT(nftContract, tokenId);
 
   const imageContainerStyle = {
-    backgroundColor: "#DDD8FC", // Background color for the image section
-    borderRadius: "12px 12px 0 0", // Rounded corners only at the top
-    overflow: "hidden", // Hide any overflow
+    backgroundColor: "#DDD8FC",
+    borderRadius: "12px 12px 0 0",
+    overflow: "hidden",
   };
 
   const contentContainerStyle: React.CSSProperties = {
-    backgroundColor: "#202020", // Background color for the content section
-    borderRadius: "0 0 12px 12px", // Rounded corners only at the bottom
-    padding: "20px", // Added padding for spacing
-    textAlign: "center", // Center the text
+    backgroundColor: "#202020",
+    borderRadius: "0 0 12px 12px",
+    padding: "20px",
+    textAlign: "center",
+    height: "200px", // Fixed height for uniformity
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
   };
 
   const headingStyle = {
     color: "#FFF",
     fontSize: "1.6rem",
-    margin: "8px 0", // Added top and bottom margin
+    margin: "8px 0",
   };
 
   const descriptionStyle = {
     color: "#FFF",
     fontSize: "1.4rem",
-    margin: "0 0 16px 0", // Added bottom margin
-    height: "4em",
+    margin: "0 0 16px 0",
     overflow: "hidden",
     textOverflow: "ellipsis",
+    display: "-webkit-box",
+    WebkitLineClamp: 3, // Limit to 3 lines
+    WebkitBoxOrient: "vertical",
   };
 
   const buttonStyle = {
@@ -72,14 +65,15 @@ const NFTCard: React.FC<Props> = ({ tokenId }) => {
   return (
     <Box
       borderRadius="12px"
-      border="1px solid #CCCCCC" // Gray border color
-      width="300px" // Reduced width
-      marginBottom="20px"
+      border="1px solid #CCCCCC"
+      width="100%"
+      maxWidth="300px"
+      margin="0 auto"
       overflow="hidden"
     >
       <Box style={imageContainerStyle}>
         <img
-          src={data?.metadata?.image || ""} // Provide an empty string as a fallback value
+          src={data?.metadata?.image || ""}
           alt={data?.metadata?.name?.toString() || ""}
           style={{ width: "100%", height: "300px", objectFit: "cover" }}
         />
@@ -101,16 +95,6 @@ const NFTCard: React.FC<Props> = ({ tokenId }) => {
           </Web3Button>
         )}
       </Box>
-      {/* <button
-        onClick={() =>
-          claimTokens({
-            to: String(address),
-            amount: 100,
-          })
-        }
-      >
-        Claim Tokens!
-      </button> */}
     </Box>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,5 @@
 import type { NextPage } from "next";
-import {
-  Box,
-  Container,
-  Flex,
-  Heading,
-  SimpleGrid,
-  Spinner,
-} from "@chakra-ui/react";
+import { Box, Container, Flex, Spinner, SimpleGrid } from "@chakra-ui/react";
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { NFT_ADDRESS } from "../constant/addresses";
 import NFTCard from "../components/NFTCard";
@@ -29,13 +22,7 @@ const Home: NextPage = () => {
         </Flex>
       ) : (
         <Container maxW={"1100px"}>
-          <SimpleGrid
-            columns={[1, 2, 3]}
-            spacing={50}
-            my={10}
-            minChildWidth="250px"
-            width="100%"
-          >
+          <SimpleGrid columns={[1, 2, 3]} spacing={10} my={10} width="100%">
             {nfts && nfts.length > 0 ? (
               nfts.map((nft, index) => (
                 <NFTCard key={index} tokenId={nft.metadata.id} />
