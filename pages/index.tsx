291727d1fa498a403cbbea6e04214ca024c06b8f
Anant Jain <ritujainanant2810@gmail.com>
2024-06-22 16:21:32 +0530

    NFT updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,18 @@ import {
   SimpleGrid,
   Spinner,
 } from "@chakra-ui/react";
-import { useContract, useMetadata } from "@thirdweb-dev/react";
+import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { NFT_ADDRESS } from "../constant/addresses";
 import NFTCard from "../components/NFTCard";
 
 const Home: NextPage = () => {
   const { contract } = useContract(NFT_ADDRESS);
 
-  const { data: metadata, isLoading: loadingMetadata } = useMetadata(contract);
+  const { data: nfts, isLoading: loadingNFTs } = useNFTs(contract);
 
   return (
-    <Container maxW={"1200px"}>
-      {loadingMetadata ? (
+    <Container maxW={"1100px"}>
+      {loadingNFTs ? (
         <Flex
           h={"90vh"}
           direction={"column"}
@@ -28,17 +28,21 @@ const Home: NextPage = () => {
           <Spinner />
         </Flex>
       ) : (
-        <Container maxW={"1200px"}>
+        <Container maxW={"1100px"}>
           <SimpleGrid
             columns={[1, 2, 3]}
-            spacing={10}
+            spacing={50}
             my={10}
             minChildWidth="250px"
             width="100%"
           >
-            <NFTCard tokenId={"0"} />
-            <NFTCard tokenId={"1"} />
-            <NFTCard tokenId={"2"} />
+            {nfts && nfts.length > 0 ? (
+              nfts.map((nft, index) => (
+                <NFTCard key={index} tokenId={nft.metadata.id} />
+              ))
+            ) : (
+              <Box>No NFTs found</Box>
+            )}
           </SimpleGrid>
         </Container>
       )}
